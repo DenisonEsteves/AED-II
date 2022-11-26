61103ac2c89cb3efbda7b28e3858164d0e9f4f1e
Denison Esteves <67080663+DenisonEsteves@users.noreply.github.com>
2022-11-26 18:15:18 -0300

    Update EC1_Q2.c

--- EC1_Q2.c
+++ EC1_Q2.c
@@ -1,3 +1,12 @@
+/*
+Considere um vetor contendo somente os caracteres "V" e "F", no qual todos os "V"s 
+são apresentados nas primeiras posições seguidos de todos os "F"s. Descreva um algoritmo 
+e sua implementação (em Python, C ou C++), usando o paradigma Divisão e Conquista, que apresente 
+como saída o número de "F"s existentes no vetor recebido. Por exemplo, se o algoritmo receber 
+vetor = ["V", "V", "V", "V", "F", "F", "F"] sua saída será 3, 
+já se receber vetor = ["V", "V", "V"] sua saída será 0.
+*/
+
 #include <stdio.h>
 
 #define LENGTH 7
